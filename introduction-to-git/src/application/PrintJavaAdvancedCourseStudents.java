dca22dbf60194b73a9f77a51e4e2c5f1954d9453
Soft-Academy-Course-Administrator <learn.soft.academy@gmail.com>
2016-04-13 01:06:02 +0300

    Update PrintJavaAdvancedCourseStudents.java

--- introduction-to-git/src/application/PrintJavaAdvancedCourseStudents.java
+++ introduction-to-git/src/application/PrintJavaAdvancedCourseStudents.java
@@ -28,6 +28,7 @@ public class PrintJavaAdvancedCourseStudents {
 	}
 
 	private static void addPeopleInClass(Set<Person> peopleInCourse) {
+		//Add your elements here
 		peopleInCourse.add(new DimitarApostolov());
 	}
 	
