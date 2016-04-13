441ba554de2e2726d17bf59fa51454cbcc8591b4
Soft-Academy-Course-Administrator <learn.soft.academy@gmail.com>
2016-04-13 19:26:25 +0300

    Bug fix on main

--- java-code-conventions/src/com/softacad/snippets/code/conventions/Main.java
+++ java-code-conventions/src/com/softacad/snippets/code/conventions/Main.java
@@ -3,9 +3,9 @@ package com.softacad.snippets.code.conventions;
 public class Main {
 
 	public static void main(String[] args) {
-		Airplane airplane = new Airplane(180);
+		Airplane airplane = new Airplane(180, Airplane.AIRBUS_TYPE);
 		
-		Airplane su37 = new Airplane(2, 13.2);
+		Airplane su37 = new Airplane(2, Airplane.AIRBUS_TYPE, 13.2);
 	}
 
 }
