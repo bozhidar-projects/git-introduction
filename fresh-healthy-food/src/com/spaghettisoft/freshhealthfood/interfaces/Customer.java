f23c40151461ac58aabe5c701e594fd57abc4b7a
georgiatanasov147 <georgi.atanasov147@gmail.com>
2016-04-22 22:17:38 +0300

    Revert "Product class and interface"
    This reverts commit 0a6dd868a7c0c8ee7de870a0d9dc3519f2788e5d.

--- fresh-healthy-food/src/com/spaghettisoft/freshhealthfood/interfaces/Customer.java
+++ fresh-healthy-food/src/com/spaghettisoft/freshhealthfood/interfaces/Customer.java
@@ -1,17 +1,5 @@
 package com.spaghettisoft.freshhealthfood.interfaces;
 
 public interface Customer {
-	
-	void setAddress(String address);
-	String getAddress();
-	
-	void setName(String name);
-	String getName();
-	
-	void setPhone(String phone);
-	String getPhone();
-	
-	void registerInSite();
-	void placeOrder(Product product);
-	void receiveProducts(Product product);
+
 }
--- fresh-healthy-food/src/com/spaghettisoft/freshhealthfood/interfaces/Product.java
+++ fresh-healthy-food/src/com/spaghettisoft/freshhealthfood/interfaces/Product.java
@@ -1,22 +1,5 @@
 package com.spaghettisoft.freshhealthfood.interfaces;
 
 public interface Product {
-	
-	void setType(String type);
-	String getType();
-	
-	void setName(String name);
-	String getName();
-	
-	void setPrice(double price);
-	double getPrice();
-	
-	boolean isAvailable();
-	void setAvailability(boolean avbl);
-	
-	void rate(int stars);
-	int getRaiting();
-	
-	void setFarmer(Farmer farmer);
-	Farmer getFarmer();
+
 }
