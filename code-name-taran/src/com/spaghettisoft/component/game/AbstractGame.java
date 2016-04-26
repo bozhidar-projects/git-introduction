be9da7eac0b5dda1f83e177ea0e852654f0368a8
VRaptor <ventseslav.marinov@gmail.com>
2016-04-26 20:56:52 +0300

    My Changes

---
+++ code-name-taran/src/com/spaghettisoft/component/game/AbstractGame.java
@@ -0,0 +1,25 @@
+package com.spaghettisoft.component.game;
+
+import com.spaghettisoft.component.Component;
+
+public abstract class AbstractGame implements Component {
+
+	@Override
+	public void show() {
+
+		while (!isEnded()) {
+			drawGame();
+			processGame();
+		}
+		printEndGameMessage();
+	}
+
+	protected abstract void printEndGameMessage();
+
+	protected abstract void processGame();
+
+	protected abstract void drawGame();
+
+	protected abstract boolean isEnded();
+
+}
