291a2c3c9281cd5c06f07f0949503feac7564eff
Ana Burnaska <ahristova.milenova@gmail.com>
2016-04-26 21:10:16 +0300

    Local changes

---
+++ code-name-taran/src/com/spaghettisoft/component/menu/HangmanMenu.java
@@ -0,0 +1,14 @@
+package com.spaghettisoft.component.menu;
+
+import java.util.List;
+
+import com.spaghettisoft.component.menu.items.MenuItem;
+
+public class HangmanMenu extends Menu {
+
+	public HangmanMenu(List<MenuItem> menuItems, String exitOptionName) {
+		super(menuItems, exitOptionName);
+		// TODO Auto-generated constructor stub
+	}
+
+}
