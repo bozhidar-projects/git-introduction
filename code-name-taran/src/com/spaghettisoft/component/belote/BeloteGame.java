b208ab4b9d79792315e1dd77043d3d48d8e7bfdf
Soft-Academy-Course-Administrator <learn.soft.academy@gmail.com>
2016-06-03 22:12:24 +0300

    Hot Fix

--- code-name-taran/src/com/spaghettisoft/component/belote/BeloteGame.java
+++ code-name-taran/src/com/spaghettisoft/component/belote/BeloteGame.java
@@ -4,17 +4,18 @@ import com.spaghettisoft.component.Component;
 
 public class BeloteGame implements Component {
 
-	@Override
-	public void show() {
-		Game belotGame = new Game();
-		belotGame.printStatus(true);
-		belotGame.callTrump();
-		belotGame.finishDealing();
-		belotGame.printStatus(true);
-		belotGame.scoreBonuses();
-		belotGame.playCards();
-		belotGame.calculateScores();
-		belotGame.printStatus(false);
-	}
+    @Override
+    public void show() {
+        Game belotGame = new Game();
+        belotGame.dealFiveCards();
+        belotGame.printStatus(true);
+        belotGame.callTrump();
+        belotGame.finishDealing();
+        belotGame.printStatus(true);
+        belotGame.scoreBonuses();
+        belotGame.playCards();
+        belotGame.calculateScores();
+        belotGame.printStatus(false);
+    }
 
 }
